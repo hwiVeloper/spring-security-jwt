5c1ff95f5979173a0927f316f14afbc4134745db
Jonghwi Lee <zziller03@gmail.com>
2020-02-24 23:45:10 +0900

    init

---
+++ src/main/java/dev/hwiveloper/ssjwt/SpringSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package dev.hwiveloper.ssjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/hwiveloper/ssjwt/SpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hwiveloper.ssjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
