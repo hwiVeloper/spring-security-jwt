d141878ad5b954c40fadeb4551e5b556d09a4674
Jonghwi Lee <zziller03@gmail.com>
2020-02-29 00:59:26 +0900

    jwt

--- src/main/java/dev/hwiveloper/ssjwt/SecurityConfigurer.java
+++ src/main/java/dev/hwiveloper/ssjwt/SecurityConfigurer.java
@@ -2,12 +2,17 @@ package dev.hwiveloper.ssjwt;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import dev.hwiveloper.ssjwt.filters.JwtRequestFilter;
 import dev.hwiveloper.ssjwt.services.MyUserDetailsService;
 
 @EnableWebSecurity
@@ -15,9 +20,28 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private MyUserDetailsService myUserDetailsService;
 	
+	@Autowired
+	private JwtRequestFilter filter;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(myUserDetailsService);
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+			.authorizeRequests().antMatchers("/authenticate").permitAll()
+			.anyRequest().authenticated()
+			.and().sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+	}
+	
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
 	}
 	
 	@Bean
---
+++ src/main/java/dev/hwiveloper/ssjwt/filters/JwtRequestFilter.java
@@ -0,0 +1,56 @@
+package dev.hwiveloper.ssjwt.filters;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import dev.hwiveloper.ssjwt.services.MyUserDetailsService;
+import dev.hwiveloper.ssjwt.util.JwtUtil;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter{
+	
+	@Autowired
+	private MyUserDetailsService userDetailsService;
+	
+	@Autowired
+	private JwtUtil jwtUtil;
+	
+	// Interceptor의 기능을 수행
+	// header체크
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		final String authorizationHeader = request.getHeader("Authorization");
+		
+		String username = null;;
+		String jwt = null;
+		
+		if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+			jwt = authorizationHeader.substring(7);
+			username = jwtUtil.extractUsername(jwt);
+		}
+		
+		if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+			UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+			if (jwtUtil.validateToken(jwt, userDetails)) {
+				UsernamePasswordAuthenticationToken upat = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+				upat.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+				SecurityContextHolder.getContext().setAuthentication(upat);
+			}
+		}
+		
+		filterChain.doFilter(request, response);
+	}
+}
